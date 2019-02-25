f951a0b17da99456991bc0f96773d01a1ccbfeec
student <student@LAB-DBX-2>
2019-02-25 15:20:21 +0100

    Exercise 3.

--- ZZKG-1-4/kernel.cu
+++ ZZKG-1-4/kernel.cu
@@ -5,44 +5,42 @@
 #include <iostream>
 #include "kernel.h"
 
-const int N = 32;
-const int M = 64;
+const int N = 512;
 const int blockSize = 16;
 
-__global__ void add_matrix(float *a, int N, int M)
+__global__ void generate_matrix(float *a, int N)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
 
 	int index = i + j * N;
 
-	if (i < N && j < M) {
-		a[index] += j;
+	if (i < N && j < N) {
+		a[index] = i * j;
 	}
 }
 
 int main()
 {
-	float *a = new float[N*M];
+	float *a = new float[N*N];
 
-	for (int i = 0; i < N*M; ++i) {		a[i] = 0.0f; 	}
+	for (int i = 0; i < N*N; ++i) {		a[i] = 0.0f; 	}
 	float *ad;
-	const int size = N * M * sizeof(float);
+	const int size = N * N * sizeof(float);
 	cudaMalloc((void**)&ad, size);
 	
 	cudaMemcpy(ad, a, size, cudaMemcpyHostToDevice);
 	dim3 dimBlock(blockSize, blockSize);
-	const int gridN = N / dimBlock.x + ((N % blockSize) != 0);
-	const int gridM = M / dimBlock.y + ((M % blockSize) != 0);
-	dim3 dimGrid(gridN, gridM);
-	add_matrix <<<dimGrid, dimBlock >> > (ad, N, M);
+	const int gridSize = N / dimBlock.x + ((N % blockSize) != 0);
+	dim3 dimGrid(gridSize, gridSize);
+	generate_matrix<<<dimGrid, dimBlock >>>(ad, N);
 	cudaMemcpy(a, ad, size, cudaMemcpyDeviceToHost);
 	cudaFree(ad);
 
-	for (int j = 0; j < M; ++j) {
+	for (int j = 0; j < N; ++j) {
 		for (int i = 0; i < N; ++i) {
 			const int index = j * N + i;
-			std::cout << a[index];
+			std::cout << a[index] << " ";
 		}
 		std::cout << "\n";
 	}
