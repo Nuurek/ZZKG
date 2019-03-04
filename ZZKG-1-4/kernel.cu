0186930b9c1a703dc7e1c94aef758dca02a26433
student <student@LAB-DBX-2>
2019-03-04 16:21:47 +0100

    Exercise 6.

--- ZZKG-1-4/kernel.cu
+++ ZZKG-1-4/kernel.cu
@@ -9,87 +9,43 @@
 #include <cmath>
 #include "kernel.h"
 
-const int N = 512;
-const int blockSize = 16;
+const size_t arraySize = 5000;
+const size_t blockSize = 16;
+const size_t shift = 10;
 
-struct Input {
-	float a;
-	float b;
-	float c;
-
-	void print() {
-		std::cout << "a: " << a << ", b: " << b << ", c: " << c << "\n";
-	}
-};
-
-struct Output {
-	size_t numberOfSolutions;
-	float firstSolution;
-	float secondSolution;
-	float extremum;
-
-	void print() {
-		std::cout << "number of solutions: " << numberOfSolutions;
-		if (numberOfSolutions > 0) {
-			std::cout << ", first solution: " << firstSolution;
-		}
-		if (numberOfSolutions > 1) {
-			std::cout << ", second solution: " << secondSolution;
-		}
-		std::cout << ", extremum: " << extremum << "\n";
-	}
-};
-
-__global__ void calculate(Input* inputData, Output* outputData)
+__global__ void calculate(float *input, float *output)
 {
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 
-	Input input = inputData[index];
-
-	const float delta = input.b * input.b - 4 * input.a * input.c;
-
-	Output output;
-	if (delta < 0) {
-		output.numberOfSolutions = 0;
-	} else if (delta == 0) {
-		output.numberOfSolutions = 1;
-		output.firstSolution = -input.b / (2 * input.a);
-	} else {
-		output.numberOfSolutions = 2;
-		const float deltaSquared = std::sqrt(delta);
-		output.firstSolution = (-input.b - deltaSquared) / (2 * input.a);
-		output.secondSolution = (-input.b + deltaSquared) / (2 * input.a);
+	if (index < arraySize) {
+		output[(index + shift) % arraySize] = input[index];
 	}
-	output.extremum = -delta / (4 * input.a);
-
-	outputData[index] = output;
 }
 
 int main()
 {
-	thrust::host_vector<Input> hostInput(N);
+	thrust::host_vector<float> hostInput(arraySize);
 
-	for (auto& input : hostInput) {
-		input.a = std::rand() % 10 + 1;
-		input.b = std::rand() % 10 + 1;
-		input.c = std::rand() % 10 + 1;
+	for (size_t i = 0; i < arraySize; i++) {
+		hostInput[i] = i;
+		std::cout << hostInput[i] << ", ";
 	}
+	std::cout << "\n";
 
-	thrust::device_vector<Input> deviceInput = hostInput;
-	thrust::device_vector<Output> deviceOutput(N);
+	thrust::device_vector<float> deviceInput = hostInput;
+	thrust::device_vector<float> deviceOutput(arraySize);
 
-	const int numberOfThreads = N;
 	dim3 dimBlock(blockSize);
-	const int gridSize = numberOfThreads / dimBlock.x + ((numberOfThreads % blockSize) != 0);
+	const int gridSize = arraySize / dimBlock.x + ((arraySize % dimBlock.x) != 0);
 	dim3 dimGrid(gridSize);
 	calculate<<<dimGrid, dimBlock >>> (deviceInput.data().get(), deviceOutput.data().get());
 
-	thrust::host_vector<Output> hostOutput = deviceOutput;
+	thrust::host_vector<float> hostOutput = deviceOutput;
 
-	for (size_t i = 0; i < hostInput.size(); i++) {
-		hostInput[i].print();
-		hostOutput[i].print();
+	for (const auto& element : hostOutput) {
+		std::cout << element << ", ";
 	}
+	std::cout << "\n";
 
     return 0;
 }
