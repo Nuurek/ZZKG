817e5d340e947ca3f4d67edeb0ecaeaafababff0
Nuurek <nurskijakub@gmail.com>
2019-03-18 16:16:22 +0100

    3.3

--- ZZKG-1-4/kernel.cu
+++ ZZKG-1-4/kernel.cu
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <thrust/host_vector.h>
@@ -50,3 +51,78 @@ int main()
     return 0;
 }
 
+=======
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+#include <cstdio>
+#include <iostream>
+#include <random>
+#include <cmath>
+#include "kernel.h"
+
+const size_t arraySize = 512;
+const size_t blockSize = 4;
+
+__global__ void inclusiveScan(int *input, int* output)
+{
+	__shared__ int shared[arraySize];
+
+	const size_t threadId = threadIdx.x;
+	const size_t partSize = arraySize / blockSize;
+	for (size_t i = threadId * partSize; i < (threadId + 1) * partSize; i++) {
+		shared[i] = input[i];
+	}
+	
+	__syncthreads();
+
+	int previousSum = 0;
+
+	for (size_t i = 0; i < threadId * partSize; i++) {
+		previousSum += shared[i];
+	}
+
+	__syncthreads();
+
+	for (size_t i = threadId * partSize; i < (threadId + 1) * partSize; i++) {
+		previousSum += shared[i];
+		shared[i] = previousSum;
+	}
+
+	__syncthreads();
+
+	for (size_t i = threadId * partSize; i < (threadId + 1) * partSize; i++) {
+		output[i] = shared[i];
+	}
+}
+
+int main()
+{
+	thrust::host_vector<int> hostInput(arraySize);
+
+	for (size_t i = 0; i < arraySize; i++) {
+		hostInput[i] = i;
+		std::cout << hostInput[i] << ", ";
+	}
+	std::cout << "\n";
+
+	thrust::device_vector<int> deviceInput = hostInput;
+	thrust::device_vector<int> deviceOutput(arraySize);
+
+	dim3 dimBlock(blockSize);
+	dim3 dimGrid(1);
+	inclusiveScan << <dimGrid, dimBlock>> > (deviceInput.data().get(), deviceOutput.data().get());
+
+	thrust::host_vector<int> hostOutput = deviceOutput;
+
+	for (const auto& element : hostOutput) {
+		std::cout << element << ", ";
+	}
+	std::cout << "\n";
+
+    return 0;
+}
+
+>>>>>>> 3.3
