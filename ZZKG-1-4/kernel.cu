1353b742c64e69b905b4c959163d3af94769f0db
Nuurek <nurskijakub@gmail.com>
2019-04-15 13:36:12 +0200

    4.1

--- ZZKG-1-4/kernel.cu
+++ ZZKG-1-4/kernel.cu
@@ -1,144 +1,41 @@
-<<<<<<< HEAD
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-
-#include <cstdio>
-#include <iostream>
+#include <thrust/scan.h>
+#include <thrust/reduce.h>
+#include <thrust/sort.h>
+#include <thrust/functional.h>
+#include <thrust/copy.h>
+#include <thrust/device_ptr.h>
 #include <random>
-#include <cmath>
-#include "kernel.h"
+#include <iostream>
 
-const size_t arraySize = 72363;
-const size_t blockSize = 16;
+const size_t vectorSize = 16;
 
-__global__ void calculate(int k, int *data)
+struct myMax : public thrust::binary_function<int, int, int>
 {
-	int index = k * (blockIdx.x * blockDim.x + threadIdx.x);
+	__host__ __device__ int operator()(int x, int y) {
+		return x < y ? y : x;
+	}
+};
 
-	data[index] = data[index] + data[index + k];
-}
+int main() {
+	thrust::host_vector<int> hostInput(vectorSize);
 
-int main()
-{
-	thrust::host_vector<int> hostInput(arraySize);
-
-	for (size_t i = 0; i < arraySize; i++) {
-		hostInput[i] = i;
-		std::cout << hostInput[i] << ", ";
+	for (auto& element : hostInput) {
+		element = std::rand();
+		std::cout << element << ", ";
 	}
 	std::cout << "\n";
 
 	thrust::device_vector<int> deviceInput = hostInput;
-	thrust::device_vector<int> deviceOutput(arraySize);
+	thrust::device_vector<int> deviceOutput(vectorSize);
 
-	for (int k = 1; k < arraySize; k *= 2) {
-		const size_t numberOfThreads = arraySize / k;
-		dim3 dimBlock(blockSize);
-		const int gridSize = numberOfThreads / dimBlock.x + ((numberOfThreads % dimBlock.x) != 0);
-		dim3 dimGrid(gridSize);
-		calculate << <dimGrid, dimBlock >> > (k, deviceInput.data().get());
+	thrust::inclusive_scan(deviceInput.begin(), deviceInput.end(), deviceOutput.begin(), myMax());
 
-		thrust::host_vector<int> hostOutput = deviceInput;
-
-		for (const auto& element : hostOutput) {
-			std::cout << element << ", ";
-		}
-		std::cout << "\n";
+	for (const auto& element : deviceOutput) {
+		std::cout << element << ", ";
 	}
+	std::cout << "\n";
 
-    return 0;
+	return 0;
 }
-
-=======
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-
-#include <cstdio>
-#include <iostream>
-#include <random>
-#include <cmath>
-#include "kernel.h"
-
-int factorial(unsigned int n) {
-	unsigned int result = 1;
-
-	for (size_t i = 2; i <= n; i++) {
-		result *= i;
-	}
-
-	return result;
-}
-
-void initializeRoute(unsigned int *route, size_t n) {
-	for (size_t i = 0; i < n; i++) {
-		route[i] = i;
-	}
-}
-
-const size_t N = 5;
-const size_t numberOfPermutations = factorial(N);
-unsigned int initialRoute[N];
-
-__global__ void salesman(unsigned int *initialRoute, unsigned int n, unsigned int* output)
-{
-	const size_t threadId = threadIdx.x;
-	const size_t outputIndex = threadId * n;
-	unsigned int *factoradic = new unsigned int[n];
-
-	int x = threadId;
-	for (size_t i = 1; x != 0; i++) {
-		factoradic[n - i] = x % i;
-		x = x / i;
-	}
-
-	unsigned int * route = new unsigned int[n];
-	for (size_t i = 0; i < n; i++) {
-		//output[outputIndex + i] = route[factoradic[i]];
-
-		//for (size_t j = factoradic[i]; j < n - 1; j++) {
-
-		//}
-		output[outputIndex + i] = factoradic[i];
-	}
-
-	delete[] factoradic;
-}
-
-int main()
-{
-	//thrust::host_vector<int> hostInput(arraySize);
-
-	//for (size_t i = 0; i < arraySize; i++) {
-	//	hostInput[i] = i + 1;
-	//	std::cout << hostInput[i] << ", ";
-	//}
-	//std::cout << "\n";
-
-	//thrust::device_vector<int> deviceInput = hostInput;
-	const size_t arraySize = numberOfPermutations * N;
-	initializeRoute(initialRoute, N);
-	thrust::device_vector<unsigned int> deviceOutput(arraySize);
-
-	dim3 dimBlock(numberOfPermutations);
-	dim3 dimGrid(1);
-
-	salesman << <dimGrid, dimBlock>> > (initialRoute, N, deviceOutput.data().get());
-
-	thrust::host_vector<unsigned int> hostOutput = deviceOutput;
-
-	for (size_t threadId = 0; threadId < numberOfPermutations; threadId++) {
-		const size_t outputStart = threadId * N;
-		for (size_t i = 0; i < N; i++) {
-			std::cout << hostOutput[outputStart + i];
-		}
-		std::cout << "\n";
-	}
-
-    return 0;
-}
-
->>>>>>> 3.3
