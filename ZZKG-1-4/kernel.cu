9ee7ac51340e9327d96fb14df8cabd447742562d
Nuurek <nurskijakub@gmail.com>
2019-05-06 16:40:16 +0200

    Exercise 3.9. WIP

--- ZZKG-1-4/kernel.cu
+++ ZZKG-1-4/kernel.cu
@@ -13,27 +13,64 @@
 #include <random>
 #include <iostream>
 
-const size_t vectorSize = 16;
+const size_t vectorSize = 64;
+const size_t subvectorSize = 1;
+const int maxElement = 10;
 
-struct isGreaterThanTen : public thrust::binary_function<int, int, int>
-{
-	__device__ int operator()(int x) {
-		return x > 10;
+__global__ void isSubvectorStartingOnIndex(int* vector, int vectorLength, int* subvector, int subvectorLength, bool* output) {
+	const size_t index = threadIdx.x;
+
+	bool result = true;
+	for (size_t i = index; i < index + subvectorLength; i++) {
+		if (i >= vectorLength || vector[i] != subvector[i]) {
+			result = false;
+			break;
+		}
 	}
-};
+
+	output[index] = result;
+}
 
 int main() {
-	thrust::host_vector<int> hostInputNumbers(vectorSize);
-
-	for (size_t i = 0; i < vectorSize; i++) {
-		hostInputNumbers[i] = std::rand() % 32;
-		std::cout << hostInputNumbers[i]  << ", ";
+	thrust::host_vector<int> hostVector(vectorSize);
+	for (auto& element : hostVector) {
+		element = std::rand() % maxElement;
+		std::cout << element;
 	}
 	std::cout << "\n";
 
+	thrust::host_vector<int> hostSubvector(subvectorSize);
+	for (auto& element : hostSubvector) {
+//		element = std::rand() % maxElement;
+		element = 2;
+		std::cout << element;
+	}
+	std::cout << "\n";
+
+	thrust::device_vector<int> deviceVector = hostVector;
+	thrust::device_vector<int> deviceSubvector = hostSubvector;
+
+	thrust::device_vector<bool> deviceResult(vectorSize);
+
+	dim3 dimBlock(vectorSize);
+	dim3 dimGrid(1);
+
+	isSubvectorStartingOnIndex<<<dimGrid, dimBlock>>>(
+		deviceVector.data().get(),
+		vectorSize,
+		deviceSubvector.data().get(),
+		subvectorSize,
+		deviceResult.data().get()
+	);
+
+	for (const auto& element : deviceResult) {
+		std::cout << element << ", ";
+	}
+	std::cout << "\n";
+/*
 	thrust::counting_iterator<int> deviceFirstIndex = thrust::make_counting_iterator(0);
 	thrust::counting_iterator<int> deviceLastIndex = thrust::make_counting_iterator((int)vectorSize);
-	thrust::device_vector<int> deviceInputNumbers = hostInputNumbers;
+	thrust::device_vector<int> deviceInputNumbers = hostVector;
 	thrust::device_vector<int> deviceOutput(vectorSize);
 	thrust::copy_if(
 		deviceFirstIndex,
@@ -49,6 +86,6 @@ int main() {
 		std::cout << element << ", ";
 	}
 	std::cout << "\n";
-
+*/
 	return 0;
 }
