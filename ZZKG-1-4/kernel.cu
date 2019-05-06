abc7e5cbe319f8b16e0892d7c0c187d0efc11850
Nuurek <nurskijakub@gmail.com>
2019-05-06 15:38:18 +0200

    Exercise 3.2.

--- ZZKG-1-4/kernel.cu
+++ ZZKG-1-4/kernel.cu
@@ -1,3 +1,6 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #include <thrust/scan.h>
@@ -11,10 +14,15 @@
 
 const size_t vectorSize = 16;
 
-struct myMax : public thrust::binary_function<int, int, int>
+__global__ void mapToBits(int* input, int length, int* output) {
+	const size_t index = threadIdx.x;
+	output[index] = __popc(input[index]);
+}
+
+struct sumBits : public thrust::binary_function<int, int, int>
 {
-	__host__ __device__ int operator()(int x, int y) {
-		return x < y ? y : x;
+	__device__ int operator()(int x, int y) {
+		return x + y;
 	}
 };
 
@@ -30,12 +38,19 @@ int main() {
 	thrust::device_vector<int> deviceInput = hostInput;
 	thrust::device_vector<int> deviceOutput(vectorSize);
 
-	thrust::inclusive_scan(deviceInput.begin(), deviceInput.end(), deviceOutput.begin(), myMax());
+	dim3 dimBlock(vectorSize);
+	dim3 dimGrid(1);
+	mapToBits<<<dimGrid, dimBlock>>>(deviceInput.data().get(), vectorSize, deviceOutput.data().get());
 
-	for (const auto& element : deviceOutput) {
-		std::cout << element << ", ";
+	thrust::host_vector<int> hostBits = deviceOutput;
+	for (const auto& bits : hostBits) {
+		std::cout << bits << ", ";
 	}
 	std::cout << "\n";
+	thrust::device_vector<int> deviceBits = hostBits;
+
+	int summedBits = thrust::reduce(deviceBits.begin(), deviceBits.end(), 0);
+	std::cout << summedBits << "\n";
 
 	return 0;
 }
